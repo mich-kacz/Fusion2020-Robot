ec728f0ce7cfab2c6d8f5804856191dd95116c9e
mich-kacz <62250257+mich-kacz@users.noreply.github.com>
2020-11-15 22:34:42 +0000

    Add files via upload

--- fusion2020/fusion2020/client_udp.py
+++ fusion2020/fusion2020/client_udp.py
@@ -11,29 +11,36 @@ class Camera_capture():
 		self.Client_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.server_ip=input('Podaj ip serwera - ')
 		self.adress=5006
-	
 
-	def camera_start(self):
+	
+	
+	def send_image(self, frame):# funkcja ktora wysyla odebrany obraz
 		MAX_DGRAM=2**16
 		MAX_IMG_DGRAM=MAX_DGRAM-64
+		#Compress and divide on segments
+		comp_frame=cv2.imencode('.jpg', frame)[1]
+		data=comp_frame.tostring()
+		size=len(data)
+		numb_segments=math.ceil(size/(MAX_IMG_DGRAM))
+		temp=0
+		while numb_segments:
+			temp2=min(size, temp + MAX_IMG_DGRAM)
+			self.Client_socket.sendto(struct.pack("B", numb_segments) + data[temp:temp2], (self.server_ip, self.adress))
+			temp=temp2
+			numb_segments-=1
+	
+
+	def camera_start(self): #Funkcja ktora odbiera obraz z kamery
 		cap=cv2.VideoCapture(0, cv2.CAP_V4L2)
 		while(cap.isOpened()):
 			_, frame=cap.read()
-			
-			#Compress and divide on segments
-			comp_frame=cv2.imencode('.jpg', frame)[1]
-			data=comp_frame.tostring()
-			size=len(data)
-			numb_segments=math.ceil(size/(MAX_IMG_DGRAM))
-			temp=0
-			while numb_segments:
-				temp2=min(size, temp + MAX_IMG_DGRAM)
-				self.Client_socket.sendto(struct.pack("B", numb_segments) + data[temp:temp2], (self.server_ip, self.adress))
-				temp=temp2
-				numb_segments-=1
+			self.send_image(frame)	
+
 		cap.release()
 		cv2.destroyAllWindows()
 		self.Client_socket.close()
+		
+		
 
 def main():
     print('Hi from robot.')
